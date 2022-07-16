61501dce37888f1cec23fe2cc0c10ad9816d6b15
jhnam <skawlgh112@naver.com>
2022-07-16 18:40:16 +0900

    설치 파일 추가

---
+++ install.sh
@@ -0,0 +1,11 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install git
+brew install --cask hiddenbar
+brew install --cask 1password
+brew install --cask slack
+brew install --cask intellij-idea
+brew install --cask pycharm
+brew install openjdk@17
+brew install openjdk@11
+brew install python3.8
